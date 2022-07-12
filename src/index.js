231f7474bc9cec4457a4e156a8a2a994af306f31
Samuel Jędrzejewski <samuel.jedrzejewski@carrier.com>
2022-07-12 08:40:40 +0200

    Created state for the squares

--- src/index.js
+++ src/index.js
@@ -4,10 +4,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null
+        };
+    }
+
     render() {
       return (
-        <button className="square" onClick={() => console.log(this.props.value)}>
-          {this.props.value}
+        <button
+            className="square"
+            onClick={() => this.setState({value: 'X'})}
+        >
+          {this.state.value}
         </button>
       );
     }
