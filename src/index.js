ad46c8f22748f5cc87a476a2f06fc09d6cedbffb
Samuel Jędrzejewski <samuel.jedrzejewski@carrier.com>
2022-07-12 09:27:23 +0200

    Created a function to check the winner

--- src/index.js
+++ src/index.js
@@ -84,6 +84,53 @@ class Game extends React.Component {
         );
     }
 }
+
+function checkWinner(squares) {
+    for(let row = 0; row < 3; row++) {
+        let player = '-';
+        let streak = 0;
+
+        for(let col = 0; col < 3; col++) {
+            let value = squares[row*3 + col];
+            streak = (value === null || player !== value) ? 1 : streak + 1;
+
+            player = value;
+            if(streak === 3) return player;
+        }
+    }
+
+    for(let col = 0; col < 3; col++) {
+        let player = '-';
+        let streak = 0;
+
+        for(let row = 0; row < 3; row++) {
+            let value = squares[row*3 + col];
+            streak = (value === null || player !== value) ? 1 : streak + 1;
+
+            player = value;
+            if(streak === 3) return player;
+        }
+    }
+
+    for(let i = 0; i < 2; i++) {
+        for(let row = 0; row < 3; row++) {
+            for(let col = 0; col < 3; col++) {
+                let player = '-';
+                let streak = 0;
+
+                for(let tempCol = col, tempRow = row; tempCol < 3 && tempRow < 3; i === 0 ? tempCol++ : tempCol--, tempRow++) {
+                    let value = squares[tempRow*3 + tempCol];
+                    streak = (value === null || player !== value) ? 1 : streak + 1;
+
+                    player = value;
+                    if(streak === 3) return player;
+                }
+            }
+        }
+    }
+
+    return null;
+}
   
 // ========================================
   
